c7bf2b8e6b8d257e9e08a8562d82eed3d7554536
Dr Alban THIKA <alban_thika@msn.com>
2025-11-29 13:41:29 +0100

    Add official PARAGON dimensions list

---
+++ utils/paragon_constants.py
@@ -0,0 +1,12 @@
+# utils/paragon_constants.py
+
+# Official PARAGON® dimension list – this must match the frontend exactly.
+PARAGON_DIMENSIONS = [
+    "Policy Engagement & Expertise",
+    "Accountability & Transparency",
+    "Representation & Responsiveness",
+    "Assertiveness & Influence",
+    "Governance & Institutional Strength",
+    "Organizational & Party Cohesion",
+    "Narrative & Communication",
+]
